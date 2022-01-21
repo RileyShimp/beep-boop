565bdcad947b5513f0a91db3e73a3e81f6d6f331
Riley Shimp <rilryshimp@gmail.com>
2022-01-21 14:36:37 -0800

    WIP: Add second function to return output in reverse order

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,30 @@ function beepBoop(input) {
   return outputArray;
 }
 
+function beepBoopReverse(input) {
+  let outputArray = [];
+  for (let i = i; i = 0; i--) {
+    if (i.toString().includes("3")) {
+      outputArray.push("Won't you be my neighbor?")
+    } else if (i.toString().includes("2")) {
+      outputArray.push("Boop!")
+    } else if (i.toString().includes("1")) {
+      outputArray.push("Beep!")
+    } else {
+    outputArray.push(i)
+    }
+  }
+  return outputArray;
+}
+
   // UI Logic
 
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
     const inputNumber = $("input#inputNumber").val()
-    const robogerResponse = beepBoop(inputNumber);
+    const robogerResponse = beepBoopReverse(inputNumber);
     $("#response").text(robogerResponse);
     $(".responseBox").fadeIn()
-})
+  })
 });
