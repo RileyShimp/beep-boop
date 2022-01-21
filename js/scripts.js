3f948c5ced6f2e4401046d2890bf5a8c2241c6c4
Riley Shimp <rilryshimp@gmail.com>
2022-01-21 10:42:38 -0800

    Add bootstrap and jquery files and linked to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-
+// Business Logic
 
 function beepBoop(input) {
   let outputArray = [];
   for (let i = 0; i <= input; i++) {
-
     if (i.toString().includes("3")) {
       outputArray.push("Won't you be my neighbor?")
     } else if (i.toString().includes("2")) {
@@ -16,3 +15,9 @@ function beepBoop(input) {
   }
   return outputArray;
 }
+
+  // UI Logic
+
+// $(document).ready(fuction() {
+  
+// })
